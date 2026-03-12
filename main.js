c1ea88100b7a818ba1a6ea2f294ee291400b50da
Omar Khalil <omarkhalil.personal1@gmail.com>
2026-03-12 07:26:12 +0200

    commit

---
+++ main.js
@@ -0,0 +1,113 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let Generate = document.getElementById("generatebtn");
+Generate.addEventListener("click", generate);
+
+let Pass1 = document.getElementById("pass1");
+let Pass2 = document.getElementById("pass2");
+function generate() {
+  Pass1.textContent = "";
+  Pass2.textContent = "";
+
+  for (let i = 0; i < 15; i++) {
+    let x = Math.floor(Math.random() * characters.length);
+    Pass1.textContent += characters[y];
+  }
+
+  for (let i = 0; i < 15; i++) {
+    let y = Math.floor(Math.random() * characters.length);
+    Pass2.textContent += characters[x];
+  }
+}
